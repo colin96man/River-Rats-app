5590a1cd4bf664af8e23064a8cbdfbf5014fc854
Colin Nelson <colinnelson1996@gmail.com>
2020-06-03 09:59:58 -0600

    create edit page and updating main page css styling

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
+const methodOverride = require('method-override');
 
 require('dotenv').config();
 require('./config/database');
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 app.use(session({
   secret: 'FloatTime!',
   resave: false,
