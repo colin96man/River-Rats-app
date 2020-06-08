697540d06ffb2eb8da40ca6f686931ee2b6d90ca
Colin Nelson <colinnelson1996@gmail.com>
2020-06-08 16:15:51 -0600

    date change

--- controllers/trips.js
+++ controllers/trips.js
@@ -79,7 +79,7 @@ function convertDate(isoDate) {
     const date = new Date(isoDate);
     const year = date.getFullYear();
     let month = date.getMonth()+1;
-    let dt = date.getDate()+1;
+    let dt = date.getDate();
     if (dt < 10) {
         dt = '0' + dt;
     }
